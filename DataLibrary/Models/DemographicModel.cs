805c6ad27f06fa2f7a779d6ad3b9e3ffcbb07033
Noah A Smith <nasmit19@louisville.edu>
2020-02-13 13:30:17 -0500

    Add project files.

---
+++ DataLibrary/Models/DemographicModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models
+{
+    public class DemographicModel
+    {
+        public DateTime datedTime { get; set; }
+        public int RoomId { get; set; }
+        public int UserAccount { get; set; }
+        public DateTime UserTime { get; set; }
+        public int Male { get; set; }
+        public int Female { get; set; }
+        public int Caucasian { get; set; }
+        public int AfricanAmerican { get; set; }
+        public int Other { get; set; }
+    }
+}
---
+++ SRCDB/dbo/Tables/DemographForm.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[DemographForm]
+(
+	[datedTime] DATETIME NOT NULL PRIMARY KEY, 
+    [RoomId] INT NOT NULL, 
+    [UserAccount] INT NOT NULL, 
+    [UserTime] DATETIME NOT NULL, 
+    [Male] INT NOT NULL, 
+    [Female] INT NOT NULL, 
+    [Caucasian] INT NOT NULL, 
+    [AfricanAmerican] INT NOT NULL, 
+    [Other] INT NOT NULL, 
+    CONSTRAINT [FK_DemographForm_Room] FOREIGN KEY (RoomId) REFERENCES Room(RoomId)
+)
